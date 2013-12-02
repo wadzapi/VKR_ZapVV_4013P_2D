c48d67a7b9b41b7edda1e96c2846159d88d47208
wadzapi <wadzapi@gmail.com>
2013-12-02 18:33:26 +0400

    Доработано YAWL (нужно еще)

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ TARGET=VKR_ZapVV_4013P_2D
 
 make clean &>/dev/null
 cat ./bib/*.bib > ${TARGET}.bib
-make pdf && cp -f ${TARGET}.pdf ./out/ && evince ${TARGET}.pdf
+make pdf && cp -f ${TARGET}.pdf ./out/ && cp -f ${TARGET}.log ./tmp/ && evince ${TARGET}.pdf
 make clean &>/dev/null
